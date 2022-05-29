db457de6ea88d0e0e686b0201dbf669edcec2787
SEUNGYEON-sy <cocorico127@naver.com>
2022-05-29 17:47:39 +0900

    Add Python File [Codingon Directory]

---
+++ for4.py
@@ -0,0 +1,6 @@
+user_input = input('안녕을 반복할 횟수를 입력하세요.')
+
+i = int(user_input)
+
+for i in range ( i ):
+    print('안녕')
---
+++ for5.py
@@ -0,0 +1,5 @@
+user_input = input('출력 희망 숫자를 입력하세요.')
+i = int(user_input)
+
+for i in range (1, i+1):
+    print(i)
