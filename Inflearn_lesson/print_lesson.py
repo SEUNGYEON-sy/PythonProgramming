38aed04e630f75db8694266a028ce504dd29b2b4
SEUNGYEON-sy <cocorico127@naver.com>
2022-06-12 17:51:48 +0900

    Inflearn_lesson

---
+++ Inflearn_lesson/print_lesson.py
@@ -0,0 +1,35 @@
+Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: /Users/seungyeonlee/Documents/rand_test.py =========
+7
+print()
+
+print()
+
+pritn()
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    pritn()
+NameError: name 'pritn' is not defined. Did you mean: 'print'?
+print()
+
+print('Hello World!')
+Hello World!
+
+
+print(1)
+1
+
+print([1,2,3])
+[1, 2, 3]
+
+print(1, 2, 3)
+1 2 3
+print(123)
+123
+print('python')
+python
+
+print('My name is', ' Seungyeon')
+My name is  Seungyeon
